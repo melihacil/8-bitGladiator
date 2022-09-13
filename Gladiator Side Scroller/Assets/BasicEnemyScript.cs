3f4556aa5453233fbf06017a935078471656ec9c
melihacil <knghtmellomello@gmail.com>
2022-09-13 15:12:39 +0300

    Update BasicEnemyScript.cs

--- Gladiator Side Scroller/Assets/BasicEnemyScript.cs
+++ Gladiator Side Scroller/Assets/BasicEnemyScript.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//This script is not used anymore
+
 public class BasicEnemyScript : MonoBehaviour
 {
     // Start is called before the first frame update
